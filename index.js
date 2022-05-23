692bb51f8dc83e6320676a5f4cf9caaa93055b01
DylanParenzee <dylanparenzee@hotmail.com>
2022-05-23 17:31:58 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+ 
+
+const writeCards = (arrNames, event) => {
+    const newArr = [];
+    for(let i=0; i<arrNames.length; i++){
+        let greetings = `Thank you, ${arrNames[i]}, for the wonderful ${event} gift!`
+        newArr.push(greetings)
+        
+
+    } return newArr
+}
+
+
+
+
+
+
+
+let countDown = (number) =>{
+    while(number >= 0){
+        console.log(number--)
+    
+    }
+
+}
